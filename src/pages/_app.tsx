8b33db1672ce4d97f06ad54069c52f4a0445bda9
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-04-11 16:14:31 -0300

    configuração do chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import {AppProps} from 'next/app'
-function MyApp({ Component, pageProps }:AppProps) {
-  return <Component {...pageProps} />
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 
-
 export default function Home() {
   return (
   <h1>Heloo baby</h1>
