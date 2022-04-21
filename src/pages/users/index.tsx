5e065dd1027401ecd6942de19594fd1f8bef6f73
Rosana Moreira <rooh.moreiraa@gmail.com>
2022-04-21 17:17:49 -0300

    configurando o axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,11 +21,11 @@ import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 import Link from "next/link";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery("users", async () => {
+    const { data } = await api.get("users");
 
     const users = data.users.map((user) => {
       return {
@@ -56,6 +56,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
